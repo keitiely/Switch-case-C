1b55935a783ce516164d08dabb79f622befba1ac
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-02 23:38:00 -0300

    Update crescente.c

--- crescente.c
+++ crescente.c
@@ -1,3 +1,6 @@
+/*1-Faça um programa que receba quatro valores, I, A, B e C. I é um valor inteiro e positivo e A, B, C são valores reais. 
+Escreva os números A, B, e C obedecendo a tabela a seguir. 
+Supondo que o valor digitado para I seja sempre um valor válido, ou seja, 1, 2 ou 3.*/
 #include <stdio.h>
 
 int main(){
