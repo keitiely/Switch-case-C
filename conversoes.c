4dc6591309a18621df049120725877b4d33e5090
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-04 17:09:36 -0300

    Update and rename conversoesss.c to conversoes.c

--- conversoes.c
+++ conversoes.c
@@ -59,7 +59,7 @@ int main(){
       result = (valor1 - 273) * 1.8 + 32;
       printf("%.1f Kelvin equivale a %.1f Fahrenheit", valor1, result);
     }
-
+    break;
     //Distancia
     case 2:
     printf("Digite o numero correspondente da unidade e para qual deseja converter, separado por espaco:\n(1) Metros\n(2) Quilometros\n(3) Milhas\n");
@@ -73,7 +73,69 @@ int main(){
     }
     //metros para milhas
     else if(origem==1 && destino==3){
-      result = valor
+      result = valor1 / 1609;
+      printf("%.1f metros equivale a %.1f milhas", valor1, result);
     }
+    //quilometros para metros
+    else if(origem==2 && destino==1){
+      result = valor1 * 1000;
+      printf("%.1f quilometros equivale a %.1f metros", valor1, result);
+    }
+    //quilometros para milhas
+    else if(origem==2 && destino==3){
+      result = valor1 / 1.609;
+      printf("%.1f quilometros equivale a %.1f milhas", valor1, result);
+    }
+    //milhas para metros
+    else if(origem==3 && destino==1){
+      result = valor1 * 1609;
+      printf("%.1f milhas equivale a %.1f metros", valor1, result);
+    }
+    //milhas para quilometros
+    else if(origem==3 && destino==2){
+      result = valor1 * 1.609;
+      printf("%.1f milhas equivale a %.1f quilometros", valor1, result);
+    }
+    break;
+    //Tempo
+    case 3:
+      printf("Digite o numero correspondente da unidade e para qual deseja converter, separado por espaco:\n(1) Horas\n(2) Minutos\n(3) Segundos\n");
+      scanf("%d %d", &origem, &destino);
+      printf("Digite o valor:\n");
+      scanf("%f", &valor1);
+    //horas para minutos
+    if(origem==1 && destino==2){
+      result = valor1 * 60;
+      printf("%.1f horas equivale a %.1f minutos", valor1, result);
+    }
+    //horas para segundos
+    else if(origem==1 && destino==3){
+      result = valor1 * 3600;
+      printf("%.1f horas equivale a %.1f segundos", valor1, result);
+    }
+    //minutos para horas
+    else if(origem==2 && destino==1){
+      result = valor1 / 60;
+      printf("%.1f minutos equivale a %.1f horas", valor1, result);
+    }
+    //minutos para segundos
+    else if(origem==2 && destino==3){
+      result = valor1 * 60;
+      printf("%.1f minutos equivale a %.1f segundos", valor1, result);
+    }
+    //segundos para horas
+    else if(origem==3 && destino==1){
+      result= valor1 / 360;
+      printf("%.1f segundos equivale a %.1f horas", valor1, result);
+    }
+    //segundos para minutos
+    else if(origem==3 && destino==2){
+      result = valor1 / 60;
+      printf("%.1f segundos equivale a %.1f minutos", valor1, result);
+    }
+    break;
+    default:
+    printf("Valor invalido!");
+    break;
   }
 }
